bd7fdc57f28dc4042e9acf105963ae622abd1b3c
jondmarco <jontheking33@hotmail.com>
2016-10-22 17:00:09 -0400

    update gamestate

--- Assets/Scripts/Game Logic/GameState.cs
+++ Assets/Scripts/Game Logic/GameState.cs
@@ -6,18 +6,13 @@ public enum GameMode {STOCK, TIMER};
 
 public class GameState : MonoBehaviour {
 
-	private static int winScore;
-
-	private static List<int> accumScore = new List<int>();
-
 	public static GameMode gameMode;
 
+	private static int winScore;
+	private static List<int> accumScore = new List<int>();
 	private static int playerCount;
 
-	public static void resetSettings() {
-		gameMode = 0;		
-	}
-
+	//returns index of winning player or -1
 	public static int isGameOver() {
 
 		for(int i = 0; i < accumScore.Count; i++) {
@@ -35,14 +30,15 @@ public class GameState : MonoBehaviour {
 			gameOverScene (winningPlayer);
 		}
 	}
-
-	private static void gameOverScene(int winningPlayer) {
-		//TODO: change to game over scene showing winning player
-	}
-
+		
 	public void initializeSettings(GameMode gameMode, int playerCount, int winScore) {
 		GameState.gameMode = gameMode;
 		GameState.playerCount = playerCount;
 		GameState.winScore = winScore;
 	}
+
+	private static void gameOverScene(int winningPlayer) {
+		//TODO: change to game over scene showing winning player
+	}
+
 }
