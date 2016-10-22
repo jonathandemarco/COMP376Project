cb42e449e864c4abc18b5cf569e4c03fafcf4b74
Sinocence <datrueaznvietz@hotmail.com>
2016-10-22 19:09:46 -0400

    TestSimon Animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,18 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+	public float moveSpeed = 5.0f;
+	
+	void Update(){
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		Vector3 direction = new Vector3 (horizontal, 0, vertical);
+		transform.position += (direction) * moveSpeed * Time.deltaTime;
+
+
+
+	}
+		
+
 }
