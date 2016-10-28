973c8d18f79b90f8737fb9e36deb7e5fc0712d34
Demo Kioussis <Demo.kioussis@gmail.com>
2016-10-28 17:43:00 -0400

    weird bug

--- Assets/Scripts/Player/InventoryManager.cs
+++ Assets/Scripts/Player/InventoryManager.cs
@@ -1,42 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class InventoryManager : MonoBehaviour {
+[System.Serializable]
+public class InventoryManager : MonoBehaviour
+{
 
-	public Weapon[] inventory;
-	public int maxInventorySize;
-	private WeaponDatabase database;
+    public Weapon[] inventory;
+    public int maxInventorySize;
+    private WeaponDatabase database;
+    public Weapon nullWeapon;
 
-	void Start(){
-		inventory = new Weapon[maxInventorySize];
-		database = GameObject.FindGameObjectWithTag ("Weapon Database").GetComponent<WeaponDatabase> ();
-	}
+    void Start()
+    {
+        inventory = new Weapon[maxInventorySize];
+        database = GameObject.FindGameObjectWithTag("Weapon Database").GetComponent<WeaponDatabase>();
+        nullWeapon = database.getNullWeapon();
+        for (int i = 0; i < maxInventorySize; i++)
+        {
+            inventory[i] = nullWeapon;
+        }
+    }
 
-	public void AddToInventory(Weapon wep){
-		for (int i = 0; i < inventory.Length; i++) {
-			if (inventory [i] == null) {
-				inventory [i] = wep;
-				break;
-			}
-		}
+    public void AddToInventory(Weapon wep)
+    {
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            if (inventory[i] == nullWeapon)
+            {
+                inventory[i] = wep;
+                break;
+            }
+        }
+    }
+    public Weapon GetWeapon(int i)
+    {
+        if (i > GetWeaponCount())
+        {
+            return nullWeapon;
+        }
+        return inventory[i];
+    }
 
-	}
+    public Weapon[] getWeaponList()
+    {
+        return inventory;
+    }
+    public void DropFromInventory(int index)
+    {
+        inventory[index] = nullWeapon;
+        //TODO Drop?
+    }
 
-	public void DropFromInventory(int index){
-		inventory [index] = null;
-		//TODO Drop?
-	}
+    public int GetWeaponCount()
+    {
+        int count = 0;
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            if (inventory[i] != nullWeapon)
+                count++;
+        }
+        return count;
+    }
 
-	public int GetWeaponCount(){
-		int count = 0;
-		for (int i = 0; i < inventory.Length; i++) {
-			if (inventory [i] != null)
-				count++;
-		}
-		return count;
-	}
-
-	public WeaponDatabase GetWeaponDatabase(){
-		return database;
-	}
+    public WeaponDatabase GetWeaponDatabase()
+    {
+        return database;
+    }
 }
