05c1886870150cf2e65bce3fca648a39f0a0b305
Cedric <Cedric@Cedrics-MBP.wireless.concordia.ca>
2016-10-29 14:25:06 -0400

    Added gun

--- Assets/Scripts/Weapon/Gun.cs
+++ Assets/Scripts/Weapon/Gun.cs
@@ -2,6 +2,15 @@
 using System.Collections;
 
 public class Gun : Weapon {
+
+	public Transform bulletSpawnPos;
+	public GameObject mLoadingPrefab;
+	public GameObject mBulletPrefab;
+	GameObject loader;
+	bool wasloaded;
+	float loadtime = 3.0f;
+	float mTimer = 0.0f;
+
 	void Start () {
 		SetType (WeaponType.Range);
 		SetDamage(5);
@@ -13,4 +22,42 @@ public class Gun : Weapon {
 		YieldAttackAnimator();
 	}
 
+	void Update () {
+		//spawnBullet ();
+		loadFire ();
+	}
+
+//	void spawnBullet () {
+//
+//		if (Input.GetKeyDown (KeyCode.Space))
+//			Instantiate (mBulletPrefab, bulletSpawnPos.position, bulletSpawnPos.rotation);
+//	}
+
+	void loadFire () {
+		
+		if (Input.GetKey (KeyCode.Space)) {
+			
+			if (!wasloaded) {
+				loader = (GameObject)Instantiate (mLoadingPrefab, bulletSpawnPos.position, bulletSpawnPos.rotation, bulletSpawnPos);
+				wasloaded = true;
+			}
+
+			mTimer += Time.deltaTime;
+			if (mTimer > loadtime) {
+				Destroy (loader);
+				Instantiate (mBulletPrefab, bulletSpawnPos.position, bulletSpawnPos.rotation);
+				reset ();
+			}
+
+		} else {
+			Destroy (loader);
+			reset ();
+		}
+			
+	}
+
+	void reset () {
+		wasloaded = false;
+		mTimer = 0.0f;
+	}
 }
---
+++ Assets/Scripts/Weapon/loadfx.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadfx : MonoBehaviour {
+
+	Light childlight;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.GetChild(0).transform.localScale += new Vector3 (0.33f, 0.33f, 0.33f) * Time.deltaTime;
+
+
+	}
+}
