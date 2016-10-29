f96ae84c3c88a7c71e44bad98b4584d739e1c992
Dexter Ho-Yiu Kwok <d_kwok@encs.concordia.ca>
2016-10-29 19:14:24 -0400

    Simon menu changes

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,19 +3,58 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
+	public bool player1Start;
+	public bool player2Start;
+	public bool player3Start;
+	public bool player4Start;
+
+	public GameObject player1_image;
+	public GameObject player2_image;
+	public GameObject player3_image;
+	public GameObject player4_image;
+
+
+	[SerializeField] int numOfPlayers = 0;
 
 	void Update(){
-		if (Input.GetKeyDown ("space")) {
-			LoadLevel ();
-		}
+		AddPlayers ();
 	}
 
-	public void LoadLevel(){
+	public void LoadLevel(int num){
+		GameState.playerCount = num;
 		SceneManager.LoadScene ("Gladiators");
 	}
 
-	public void printStuff(){
-		Debug.Log ("Hello");
+	void AddPlayers(){
+		if (Input.GetButtonDown ("A0")) {
+			if (!player1Start) {
+				numOfPlayers++;
+				player1Start = true;
+			}
+		}
+		if (Input.GetButtonDown ("A1")) {
+			if (!player2Start) {
+				numOfPlayers++;
+				player2Start = true;
+			}
+		}
+		if (Input.GetButtonDown ("A2")) {
+			if (!player3Start) {
+				numOfPlayers++;
+				player3Start = true;
+			}
+		}
+		if (Input.GetButtonDown ("A3")) {
+			if (!player4Start) {
+				numOfPlayers++;
+				player4Start = true;
+			}
+		}
+
+	}
+
+	void ShowCurrentPlayers(){
+
 	}
 
 
