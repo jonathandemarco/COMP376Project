bf8531ee864e0167bb16371baf3fe5794cdd819a
Demo Kioussis <Demo.kioussis@gmail.com>
2016-10-31 20:57:54 -0400

    nothing

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Player
+{
+    public float maxHealth;
+    public int numLives;
+    public float respawnTime;
+    public char playerChar;
+    public char team;
+}
+
---
+++ Assets/Scripts/Player/PlayerState.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class PlayerState
+{
+    public float invinsibilityTime;
+    public float invinsibilityFlickerRate;
+    public float pushbackParalizationTime;
+    public int inventorySize;
+    public float tapTime;
+}
