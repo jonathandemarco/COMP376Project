fa645a3dc1b79e38c8c16e8984d655f281a44b2c
Andres Gonzalez <wired@hotmail.com>
2016-11-01 01:00:43 -0400

    HUD inventory display working

--- Assets/Scripts/Game Logic/PlayerStatus.cs
+++ Assets/Scripts/Game Logic/PlayerStatus.cs
@@ -31,12 +31,12 @@ public class PlayerStatus : MonoBehaviour {
 			sphere.layer = LayerMask.NameToLayer ("HUD");
 			Vector3 scale = b.size * 0.5f;
 			float n = Mathf.Clamp (scale.x, 0, 2.0f);
-			sphere.transform.position = transform.position - new Vector3(- i * n * 2 / itemCount - 2 - n * 0.5f / itemCount, 2, 0);
-			sphere.transform.localScale = new Vector3 (n / itemCount, n / itemCount, 0.2f);
+			sphere.transform.position = transform.position - new Vector3(- i * n * 2 / itemCount - 2 - n * 0.5f / itemCount, 2, 2);
+			sphere.transform.localScale = new Vector3 (n / itemCount, n / itemCount, n / itemCount);
 			if (!(weapons [i] is NullWeapon)) {
 				GameObject w = Instantiate (weapons [i].gameObject, sphere.transform.position - new Vector3 (0, 0, 1), Quaternion.identity, sphere.transform) as GameObject;
 				w.layer = LayerMask.NameToLayer ("HUD");
-
+				w.transform.localScale = new Vector3 (1, 1, 1);
 				Component[] comp = w.GetComponents<Component> ();
 
 				foreach (Component c in comp) {
@@ -45,31 +45,28 @@ public class PlayerStatus : MonoBehaviour {
 				}
 			
 				Renderer[] renderers = w.GetComponentsInChildren<Renderer> ();
-				Vector3 minBounds = new Vector3 (Mathf.Infinity, Mathf.Infinity, Mathf.Infinity);
-				Vector3 maxBounds = new Vector3 (-Mathf.Infinity, -Mathf.Infinity, -Mathf.Infinity);
+
 				for (int j = 0; j < renderers.Length; j++) {
 					renderers [j].enabled = true;
 					renderers [j].gameObject.layer = LayerMask.NameToLayer ("HUD");
-					if (renderers [j].bounds.min.x < minBounds.x)
-						minBounds.x = renderers [j].bounds.min.x;
-					if (renderers [j].bounds.min.y < minBounds.y)
-						minBounds.y = renderers [j].bounds.min.y;
-					if (renderers [j].bounds.min.z < minBounds.z)
-						minBounds.z = renderers [j].bounds.min.z;
-				
-					if (renderers [j].bounds.max.x > maxBounds.x)
-						maxBounds.x = renderers [j].bounds.max.x;
-					if (renderers [j].bounds.max.y > maxBounds.y)
-						maxBounds.y = renderers [j].bounds.max.y;
-					if (renderers [j].bounds.max.z > maxBounds.z)
-						maxBounds.z = renderers [j].bounds.max.z;
 				}
 
+				Bounds bounds = new Bounds();
+				Vector3 minBounds = new Vector3 (Mathf.Infinity, Mathf.Infinity, Mathf.Infinity);
+				Vector3 maxBounds = new Vector3 (-Mathf.Infinity, -Mathf.Infinity, -Mathf.Infinity);
+
+				MeshFilter[] meshFilters = w.GetComponentsInChildren<MeshFilter> ();
+
+				for(int j = 0; j < meshFilters.Length; j++)
+					bounds.Encapsulate (meshFilters [j].mesh.bounds);
+				
+				minBounds = bounds.min;
+				maxBounds = bounds.max;
 				Vector3 size = maxBounds - minBounds;
-				float maxSize = Mathf.Max (size.x, Mathf.Max (size.y, size.z));
-				w.transform.localScale = new Vector3 (1.5f, 1.5f, 1.5f) / maxSize;
+				float maxSize = Mathf.Max (size.x, Mathf.Max(size.y, size.z));
+				w.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f) / maxSize;
 				Vector3 diff = 1.5f * (maxBounds + minBounds) / (2 * maxSize);
-				w.transform.localPosition = - new Vector3 (diff.x, diff.y, diff.z);
+				w.transform.localPosition = - new Vector3 (diff.x, diff.y, 5);
 			}
 		}
 	}
