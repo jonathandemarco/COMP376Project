7f9827c74daa9c8336977e6761eb10988f1ca075
Sinocence <datrueaznvietz@hotmail.com>
2016-11-12 16:27:10 -0500

    Added collision detection for traps

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trap : MonoBehaviour {
+
+	public int damage = 25;
+
+	void OnCollisionEnter(Collision c){
+		Collider col = c.collider; 
+		if (col.gameObject.layer == LayerMask.NameToLayer("Player")) {
+			Vector3 direction = col.transform.position - transform.position;
+			Debug.Log ("Hit by Trap");
+			col.gameObject.GetComponent<PlayerManager> ().takeDamage (damage,direction);
+		}
+	}
+}
