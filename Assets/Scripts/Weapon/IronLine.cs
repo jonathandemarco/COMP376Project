0d8e2d30f84e94e45b335fb15d1cbc48418f8254
Dexter Ho-Yiu Kwok <d_kwok@encs.concordia.ca>
2016-11-26 17:33:59 -0500

    Stuff

--- Assets/Scripts/Weapon/IronLine.cs
+++ Assets/Scripts/Weapon/IronLine.cs
@@ -7,28 +7,25 @@ public class IronLine : Weapon {
 	public GameObject origin;
 	public GameObject destination;
 	public float lineWidth;
-	public GameObject SphericalMachine;
 
 	private CapsuleCollider capsule;
 	private bool isComplete;
 
 	void Start(){
 		lineRenderer = GetComponent<LineRenderer> ();
-		capsule = gameObject.AddComponent<CapsuleCollider> ();
-
-		capsule.radius = lineWidth / 2; // same width as the line renderer
-		capsule.center = Vector3.zero; // center it to 0, 0, 0
-		capsule.direction = 2; // z-axis for easier lookAt orientation... apparently it helps!
+		lineRenderer.enabled = false;
 	}
 
 	// Use this for initialization
 	public override void Update () {
-		lineRenderer.SetPosition (0, origin.transform.localPosition);
-		lineRenderer.SetPosition (1, destination.transform.localPosition);
-		lineRenderer.SetWidth (lineWidth, lineWidth);
+		if (destination.GetComponent<Latch> ().GetUse ()) {
+			lineRenderer.enabled = true;
+			lineRenderer.SetPosition (0, origin.transform.position);
+			lineRenderer.SetPosition (1, destination.transform.position);
+			lineRenderer.SetWidth (lineWidth, lineWidth);
 
-		capsule.transform.position = origin.transform.localPosition + (destination.transform.localPosition - origin.transform.localPosition) / 2; // find the mid point
-		capsule.transform.LookAt (origin.transform.localPosition);
-		capsule.height = (destination.transform.localPosition - origin.transform.localPosition - Vector3.Normalize(destination.transform.localPosition - origin.transform.localPosition) * (SphericalMachine.GetComponent<SphereCollider>().radius * 2 + 0.1f)).magnitude;
+		} else {
+			lineRenderer.enabled = false;
+		}
 	}
 }
--- Assets/Scripts/Weapon/Latch.cs
+++ Assets/Scripts/Weapon/Latch.cs
@@ -8,39 +8,39 @@ public class Latch : Weapon {
 
 	private bool isUsed;
 	private bool isLaunched;
+	private bool alreadyCollided;
 	private float time;
 	private Vector3 initialDistance;
-	private Vector3 maxVectorDistance;
 	private Renderer [] renderers;
 
 	// Use this for initialization
 	void Start () {
 		isUsed = false;
 		isLaunched = false;
+		alreadyCollided = false;
 		time = 0.0f;
 		initialDistance = handle.transform.localPosition - transform.localPosition;
 	}
 	
 	// Update is called once per frame
-	public override void Update () {	
+	public override void Update () {
 	
 		if (isUsed) {
 			Vector3 currentDistance = handle.transform.localPosition - transform.localPosition;
-			float maxDistanceBetweenWep = (currentDistance - maxVectorDistance).magnitude;
-			float minDistanceBetweenWep = (currentDistance - initialDistance).magnitude;
+			float distanceBetweenWep = (currentDistance - initialDistance).magnitude;
 
-			if (isLaunched && maxDistanceBetweenWep < maxDistance) {
-				transform.position = handle.transform.forward * time;
+			if (isLaunched && distanceBetweenWep < maxDistance) {
+				transform.position += -handle.transform.right * time;
 				time += Time.deltaTime;
-			} else if (isLaunched && maxDistanceBetweenWep >= maxDistance) {
+			} else if (isLaunched && distanceBetweenWep >= maxDistance) {
 				// reached max distance
 				time = 0.0f;
 				time += Time.deltaTime;
 				Pull ();
-			} else if (!isLaunched && isUsed && minDistanceBetweenWep > 0) {
-				transform.position = -handle.transform.forward * time;
+			} else if (!isLaunched && isUsed && distanceBetweenWep > 1.05f) {
+				transform.position += handle.transform.right * time;
 				time += Time.deltaTime;
-			} else if (!isLaunched && isUsed && minDistanceBetweenWep <= 0){
+			} else if (!isLaunched && isUsed && distanceBetweenWep <= 1.05f){
 				for(int r = 0; r < renderers.Length; ++r){
 					renderers [r].enabled = false;
 				}
@@ -58,5 +58,36 @@ public class Latch : Weapon {
 
 	public void Pull(){
 		isLaunched = false;
+	}
+
+	public bool GetUse(){
+		return isUsed;
+	}
+
+	public override void OnCollisionEnter(Collision c)
+	{
+		Collider col = c.collider;
+
+		MessagePassingHelper.passMessageOnCollision (this, col);
+
+		if (col.gameObject.layer == LayerMask.NameToLayer ("Player")) {
+			Debug.Log ("Boom");
+			PlayerManager manager = col.gameObject.GetComponent<PlayerManager> ();
+			char colPlayerChar = getPlayerChar ();
+
+			if (manager.getPlayerChar () != colPlayerChar) {
+				if (weaponSound != null) {
+					AudioSource audioSource = GetComponent<AudioSource> ();
+					if (audioSource != null) {
+						audioSource.clip = weaponSound;
+						audioSource.Play ();
+					}
+				}
+			}
+		} else if(!alreadyCollided && isLaunched){
+			time = 0.0f;
+			alreadyCollided = true;
+			Pull ();
+		}
 	}
 }
