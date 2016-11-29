a725ea9b1958b23784daf9443466bc817bfdffd3
jondmarco <jondmarco@gmail.com>
2016-11-29 03:52:34 -0500

    fixed pillow sound and gameover scene button sound

--- Assets/GameOverController.cs
+++ Assets/GameOverController.cs
@@ -5,14 +5,24 @@ using UnityEngine.UI;
 public class GameOverController : MonoBehaviour {
 
     public Text winnerText;
+    AudioSource swordSound;
+    string levelName;
 
 	// Use this for initialization
 	void Start () {
         winnerText.text = "Sleepy Player " + GameState.winner + " Wins!";
+        swordSound = GetComponent<AudioSource>();
     }
 
     public void loadScene(string name)
     {
-        GameState.loadScene(name);
+        swordSound.Play();
+        Invoke("LoadLevelAfterSoundEnd", 2.0f);
+        levelName = name;
+    }
+
+    public void LoadLevelAfterSoundEnd()
+    {
+        GameState.loadScene(levelName);
     }
 }
